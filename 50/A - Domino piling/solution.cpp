f80937b4d2dc616a5cb75cada8445c77339e89cc
bhumit143 <bhumit00143@gmail.com>
2026-08-10 21:43:42 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int m, n;
+    cin >> m >> n;
+    // Calculate total area and divide by 2
+    cout << (m * n) / 2 << "
+"; 
+    return 0;
+}
