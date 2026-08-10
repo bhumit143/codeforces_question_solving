b6422a2d8cbabc158311ced21deb0942169a4a99
bhumit143 <bhumit00143@gmail.com>
2026-08-10 21:43:10 +0530

    Add Coins [A] from Codeforces

---
+++ 1814/A - Coins/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, k;  
+        cin >> n >> k;   
+        
+        if (n % 2 == 0) {
+            cout << "YES" << "
+";  
+        }
+        else if (k % 2 != 0) {  
+            cout << "YES" << "
+";
+        }
+        else {
+            cout << "NO" << "
+";
+        }
+    }
+    return 0;  
+}
