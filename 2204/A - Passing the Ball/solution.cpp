d1dbc861b9bad3133b23832530844af90c194880
bhumit143 <bhumit00143@gmail.com>
2026-08-10 21:42:58 +0530

    Add Passing the Ball [A] from Codeforces

---
+++ 2204/A - Passing the Ball/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        for (int i = 0; i < n; i++) {
+            if (str[i] == 'L') {
+                cout << i + 1 << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
